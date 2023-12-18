fe9a0341a504af3993855083818534c212f8ecae
MICHELLE NASIRISI <wanjikunasirisi1@gmail.com>
2023-12-18 09:58:57 +0300

    Completed the assignment

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// Using reduce() to calculate the total number of batteries
+const totalBatteries = batteryBatches.reduce((total, batch) => total + batch, 0);
+
+// Output the result
+console.log(totalBatteries); // Output: 31
